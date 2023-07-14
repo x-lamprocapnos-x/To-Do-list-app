b8deb53bfd942f1b3ea9d0d0fc1f678fd176345b
Ashley Shoben <110067052+x-lamprocapnos-x@users.noreply.github.com>
2023-07-14 16:47:26 -0400

    Update scripts.js
    - format document

--- scripts.js
+++ scripts.js
@@ -1,32 +1,32 @@
 //add a new item to the list
 
-function newListItem(){
+function newListItem() {
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
 
-    if (inputValue === ''){
+    if (inputValue === '') {
         alert('You must input a value!');
     } else {
         $('#list').append(li);
-    
-}
-//cross off an item from the list
-function crossOut(){
-    li.toggleClass('strike');
-}
-    li.on('dblclick', function crossOut(){
+
+    }
+    //cross off an item from the list
+    function crossOut() {
+        li.toggleClass('strike');
+    }
+    li.on('dblclick', function crossOut() {
         li.toggleClass('strike');
     });
-//add a delete list item button
+    //add a delete list item button
     let crossOutButton = $('<crossOutButton></crossOutButton>');
     crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
     crossOutButton.on('click', deleteListItem);
-    function deleteListItem(){
+    function deleteListItem() {
         li.addClass('delete');
     }
-//made list sortable
+    //made list sortable
     $('#list').sortable();
 }
